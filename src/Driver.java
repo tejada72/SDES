c0a236903f2ceb8a1b5f3aa7ca6fa39b560d51ed
Alex Tejada <tejada72@students.rowan.edu>
2018-10-25 20:57:46 -0400

    Issue #12
    Created XOR methor.
    Created a private method that compares 2 boolean array and returns the smaller length as an int.
    Added proper documentation

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,29 @@
-public class Driver {
-
+import java.util.*;
+/**
+ * Test the implementation of SDES
+ *
+ * @author (sdb)
+ * @version (Oct 2018)
+ */
+public class Driver
+{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SDES sdes = new SDES();
+
+        Scanner scanner = new Scanner (System.in);
+
+        String plain = "x";
+        System.out.println ("Enter plain text, or hit 'Enter' to terminate");
+        plain = scanner.nextLine();
+        byte [] cipher;
+        while (plain.length() > 0)
+        {
+            //cipher = sdes.encrypt (plain);
+            System.out.print ("Cipher is ");
+            //sdes.show(cipher);
+            //System.out.println (sdes.byteArrayToString (sdes.decrypt (cipher)));
+            System.out.println ("Enter plain text, or hit 'Enter' to terminate");
+            plain = scanner.nextLine();
+        }
     }
 }
--- src/SDES.java
+++ src/SDES.java
@@ -1,3 +1,47 @@
+/**
+ * Simplified Data Encryption Standard is a symmetric-key algorithm for the encryption of electronic data.
+ *
+ * @author Aaron Alnutt
+ * @author Matthew Moore
+ * @author Alex Tejada
+ *
+ */
 public class SDES {
+
+    /**
+     * Exclusive Or function
+     *
+     * @param x array of boolean
+     * @param y array of boolean
+     * @return the result of x XOR y
+     */
+    public boolean[] xor(boolean[] x, boolean[] y) {
+        int range = getSmallerLength(x,y);
+        int index = 0;
+        boolean[] result = new boolean[range];
+
+        while(index < range) {
+            //1 XOR 1 == 0
+            //1 XOR 0 == 1
+            //0 XOR 1 == 1
+            //0 XOR 0 == 0
+            result[index] = x[index] && !y[index] || !x[index] && y[index];
+            index++;
+        }
+        return result;
+    }
+
+    /**
+     * Return the smaller length of 2 array of booleans.
+     *
+     * @param x array of boolean
+     * @param y array of boolean
+     * @return int representing the smaller length
+     */
+    private int getSmallerLength(boolean[] x, boolean[] y) {
+        if(x.length > y.length)
+            return y.length;
+        return x.length;
+    }
 
 }
